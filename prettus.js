84263c37c4df50ad898fa02dee9b32af091c2884
Vluzrmos <vluzrmos@gmail.com>
2015-03-15 20:15:40 +0000

    Comment more

--- prettus.js
+++ prettus.js
@@ -9,8 +9,15 @@
 		'filter':'filter'
 	};
 	
+	Prettus.defaultAjaxOptions = {
+		dataType:"json"
+	};
+	
 	var helpers = {};
 	
+	/**
+	 * Performs a join in value is array with a giving separator or returns value with is not array.
+	 */
 	helpers.join = function (value, sep){
 		if(value instanceof Array){
 			return value.join(sep||',');
@@ -20,22 +27,16 @@
 		}
 	};
 	
+	/**
+	 * Encode pairs of key value to a component URL
+	 */ 
 	helpers.encodeKeyValue = function(key, value, sep){
 		return key+"="+encodeURIComponent(helpers.join(value, sep));
 	}
 	
 	/**
-	 * Performs a ajax search on the repository
-	 * @param string url path to resource repository
-	 * @param string query string to be searched
-	 * @param json prettusOptions searchFields, filter, orderBy and sortedBy
-	 * @param json ajaxOptions jQuery Ajax Options
-	 */
-	 
-	Prettus.search = function(url, query, prettusOptions, ajaxOptions){
-		return $.ajax(helpers.prepareUrl(url, query, prettusOptions), ajaxOptions|{});
-	};
-	
+	 * Prepare the url for a giving query and options
+	 */ 
 	helpers.prepareUrl = function(url, query, prettusOptions){
 		var urlComponents = [];
 		
@@ -62,6 +63,23 @@
 		return url+"?"+urlComponents.join("&");
 	};
 	
+	/**
+	 * Configurate ajax options
+	 */ 
+	helpers.prepareAjaxOptions = function(options){
+		return $.extend({}, Prettus.defaultAjaxOptions, options|{});	
+	};
+	
+	/**
+	 * Performs a ajax search on the repository
+	 * @param string url path to resource repository
+	 * @param string query string to be searched
+	 * @param json prettusOptions searchFields, filter, orderBy and sortedBy
+	 * @param json ajaxOptions jQuery Ajax Options
+	 */
+	Prettus.search = function(url, query, prettusOptions, ajaxOptions){
+		return $.ajax(helpers.prepareUrl(url, query, prettusOptions), helpers.prepareAjaxOptions(ajaxOptions));
+	};
 
 	return (window.Prettus = Prettus);
 })($);
